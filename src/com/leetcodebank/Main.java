99185ddb03458ed44383cd8554a1ac018c001892
shaowei <willyann@foxmail.com>
2021-12-22 17:43:35 +0800

    remove unnecessary files

--- src/com/leetcodebank/Main.java
+++ src/com/leetcodebank/Main.java
@@ -1,14 +1,8 @@
 package com.leetcodebank;
 
-
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(9 / 2); //向下取整
-
     }
 }
